bfb979e8cffbda1afa0ebaa317e6d2504f591728
rubengonzalezgomez <ruben.gonzalez.gomez@alumnos.upm.es>
2022-03-08 09:50:25 +0100

    Interfaces añadidas

--- contracts/CarOwnership.sol
+++ contracts/CarOwnership.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 
 import "./CarData.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "./IERC721.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
